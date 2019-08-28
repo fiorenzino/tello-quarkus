b40acb62fe6fab0c5c08e0460919696eae57b768
fiorenzo pizza <fiorenzino@gmail.com>
2019-08-28 23:16:24 +0200

    cmds

--- src/main/java/group/weict/model/enums/TelloCmd.java
+++ src/main/java/group/weict/model/enums/TelloCmd.java
@@ -28,9 +28,119 @@ public enum TelloCmd {
      */
     EMERGENCY("emergency"),
     /**
-     * Command to make the drone flip in a specified direction.
+     * Command to make the drone flip in a left direction.
      */
-    FLIP("flip");
+    FLIP_L("flip l"),
+    /**
+     * Command to make the drone flip in a right direction.
+     */
+    FLIP_R("flip r"),
+    /**
+     * Command to make the drone flip in a forward direction.
+     */
+    FLIP_F("flip f"),
+    /**
+     * Command to make the drone flip in a back direction.
+     */
+    FLIP_B("flip b"),
+
+    /**
+     * Command to make the drone fly forward by a specified distance.
+     */
+    FORWARD("forward"),
+    /**
+     * Command to make the drone fly to position x, y, z by a specified speed.
+     */
+    GO("go"),
+    /**
+     * Command to land the drone automatically.
+     */
+    LAND("land"),
+
+    /**
+     * Command to make the drone fly left by a specified distance.
+     */
+    LEFT("left"),
+
+    /**
+     * Command to make the drone fly right by a specified distance.
+     */
+    RIGHT("right"),
+    /**
+     * Command to disable the video stream.
+     */
+    STREAMOFF("streamoff"),
+
+    /**
+     * Command to enable the video stream.
+     */
+    STREAMON("streamon"),
+
+    /**
+     * Command to take off automatically.
+     */
+    TAKEOFF("takeoff"),
+    /**
+     * Command to make the drone ascend by a specified distance.
+     */
+    UP("up"),
+
+    // READ CMDS
+    /**
+     * Command to get IMU acceleration data in 0.001g.
+     */
+    READ_ACCELERATION("acceleration?"),
+    /**
+     * Command to get IMU attitude data.
+     */
+    READ_ATTITUDE("attitude?"),
+    /**
+     * Command to get the current barometer value in m.
+     */
+    READ_BARO("baro?"),
+    /**
+     * Command to get the current battery percentage.
+     */
+    READ_BATTERY("battery?"),
+
+    /**
+     * Command to get the current height in cm.
+     */
+    READ_HEIGHT("height?"),
+
+    /**
+     * Command to get the current speed in cm/s.
+     */
+    READ_SPEED("speed?"),
+
+    /**
+     * Command to get the current temperature in degrees celsius.
+     */
+    READ_TEMP("temp?"),
+
+    /**
+     * Command to get the current flight time in seconds.
+     */
+    READ_TIME("time?"),
+
+    /**
+     * Command to get distance value from tof in cm.
+     */
+    READ_TOF("tof?"),
+
+    /**
+     * Command to get the wifi SNR.
+     */
+    READ_WIFI("wifi?"),
+
+
+    //SET CMD
+
+    /**
+     * Command to set the speed.
+     */
+    SET_WIFI("speed");
+
 
     private final String cmd;
 
