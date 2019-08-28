e8a88f4099071c9461780610b1cfef26c73d32dc
fiorenzo pizza <fiorenzino@gmail.com>
2019-08-28 22:59:47 +0200

    primi cmd enums

--- src/main/java/group/weict/model/enums/TelloCmd.java
+++ src/main/java/group/weict/model/enums/TelloCmd.java
@@ -2,7 +2,26 @@ package group.weict.model.enums;
 
 public enum TelloCmd {
 
-    BACK("back");
+    /**
+     * Command to make the drone fly backwards by a specified distance.
+     */
+    BACK("back"),
+    /**
+     * Command to make the drone rotate in a counter-clockwise direction by a specified amount of degrees.
+     */
+    CCW("ccw"),
+    /**
+     * Command to make the drone enter SDK mode. This command has to be executed before executing any of the other
+     * commands.
+     */
+    COMMAND("command"),
+    /**
+     * Command to make the drone rotate in a clockwise direction by a specified amount of degrees.
+     */
+    CW("cw"),
+
+
+    ;
 
     private final String cmd;
 
